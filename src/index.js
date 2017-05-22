fa254ac3b78d81783d221da95650fe63e632a715
Awadje <michaelawad85@gmail.com>
2017-05-22 22:56:27 +0200

    Searchbar now updating state when input is entered

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-  const API_KEY = 'AIzaSyCJ4HU65aO2pxkXtRM-O31HSi4b5nXiUgU';
+const API_KEY = 'AIzaSyCJ4HU65aO2pxkXtRM-O31HSi4b5nXiUgU';
 
-  const App = () => {
-    return (
-        <div>
-          <SearchBar />
-        </div>
+const App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'Funny Cats'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+  return (
+      <div>
+        <SearchBar />
+      </div>
     );
   }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
