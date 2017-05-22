a9815297c3f27546ce7f54dde7591e99dd5d0ccb
Awadje <michaelawad85@gmail.com>
2017-05-22 16:03:17 +0200

    Render search bar in app js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
-  const API_KEY = 'AIzaSyCJ4HU65aO2pxkXtRM-O31HSi4b5nXiUgU'
+  const API_KEY = 'AIzaSyCJ4HU65aO2pxkXtRM-O31HSi4b5nXiUgU';
 
   const App = () => {
-    return <div> Hi </div>;
+    return (
+        <div>
+          <SearchBar />
+        </div>
+    );
   }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
