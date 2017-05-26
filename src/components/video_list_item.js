68dcbe0c664d19ebfa51005c2a626a61ecc0c9e5
Awadje <michaelawad85@gmail.com>
2017-05-26 15:59:29 +0200

    Setting up lay-out for video objects

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+const VideoListItem = ({video}) => {
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading"></div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
