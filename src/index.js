07f0fc9e6bc2ce8eff023b25193e9733c6199494
Awadje <michaelawad85@gmail.com>
2017-05-31 11:18:28 +0200

    Pass videoSearch to SearchBar

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class App extends Component {
       videos: [],
       selectedVideo: null
      };
+     this.videoSearch('surfboards');
   }
 
   videoSearch(term) {
@@ -22,13 +23,13 @@ class App extends Component {
         videos: videos,
         selectedVideo: videos[0]
       });
-    });    
+    });
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
